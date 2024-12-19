4ebea5186943bee6f35d9475dafaf83f6fa8e08b
nvk-lesson4 <nguyenvukien02@gmail.com>
2024-12-19 23:14:29 +0700

    nvk-lesson12

--- database/seeders/nvkSanPham.php
+++ database/seeders/nvkSanPham.php
@@ -12,77 +12,77 @@ class nvkSanPhams extends Seeder
      */
     public function run(): void
     {
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'VP01',
-            'nvknvkTenSanPham'=>'Cay phu quy',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>699000,
-            'nvknvkMaloai'=> 1,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'VP01',
+            'nvkTenSanPham'=>'Cay phu quy',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>699000,
+            'nvkMaloai'=> 1,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'VP02',
-            'nvknvkTenSanPham'=>'Cay dai phu quy',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>199000,
-            'nvknvkMaloai'=> 1,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'VP02',
+            'nvkTenSanPham'=>'Cay dai phu quy',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>199000,
+            'nvkMaloai'=> 1,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'VP03',
-            'nvknvkTenSanPham'=>'Cay Hanh Phuc',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>899000,
-            'nvknvkMaloai'=> 1,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'VP03',
+            'nvkTenSanPham'=>'Cay Hanh Phuc',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>899000,
+            'nvkMaloai'=> 1,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'VP04',
-            'nvknvkTenSanPham'=>'Cay van loc',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>799000,
-            'nvknvkMaloai'=> 1,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'VP04',
+            'nvkTenSanPham'=>'Cay van loc',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>799000,
+            'nvkMaloai'=> 1,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'PT001',
-            'nvknvkTenSanPham'=>'Cay van loc',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>150000,
-            'nvknvkMaloai'=> 1,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'PT001',
+            'nvkTenSanPham'=>'Cay van loc',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>150000,
+            'nvkMaloai'=> 1,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'PT002',
-            'nvknvkTenSanPham'=>'Cay truong sinh',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>100000,
-            'nvknvkMaloai'=> 3,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'PT002',
+            'nvkTenSanPham'=>'Cay truong sinh',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>100000,
+            'nvkMaloai'=> 3,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'PT003',
-            'nvknvkTenSanPham'=>'Cay hanh phuc',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>200000,
-            'nvknvkMaloai'=> 3,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'PT003',
+            'nvkTenSanPham'=>'Cay hanh phuc',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>200000,
+            'nvkMaloai'=> 3,
+            'nvkTrangThai'=> 0
         ]);
-        DB::table("nvknvk_sanpham")->insert([
-            'nvknvkMaSanPham'=>'PT004',
-            'nvknvkTenSanPham'=>'Cay hoa nhai',
-            'nvknvkHinhAnh'=>'',
-            'nvknvkSoLuong'=> 100,
-            'nvknvkDonGia'=>100000,
-            'nvknvkMaloai'=> 3,
-            'nvknvkTrangThai'=> 0
+        DB::table("nvk_sanpham")->insert([
+            'nvkMaSanPham'=>'PT004',
+            'nvkTenSanPham'=>'Cay hoa nhai',
+            'nvkHinhAnh'=>'',
+            'nvkSoLuong'=> 100,
+            'nvkDonGia'=>100000,
+            'nvkMaloai'=> 3,
+            'nvkTrangThai'=> 0
         ]);
     }
 }
